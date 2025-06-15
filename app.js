0faf2a66c0b92a7c703cd8eea02173e5a0259b36
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2025-06-15 05:20:52 -0700

    done

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 // );
 app.use(
   cors({
-    origin: ["https://nearmart-frontend.vercel.app/"],
+    origin: ["https://nearmart-frontend.vercel.app"],
     credentials: true,
   })
 );
